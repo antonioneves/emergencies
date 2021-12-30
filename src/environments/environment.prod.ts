5e83f7586d3af2302f1ba29f7fbc4f5ad1b2b4de
antonioneves <antonio.neves.6@gmail.com>
2021-12-30 12:05:51 +0000

    Install packages and add environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://code-challenge.patronus-group.com/getAllEmergencies',
+  authorizationBearer: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlBhdHJvbnVzIGNvZGUgY2hhbGxlbmdlIiwiaWF0IjoxNTE2MjM5MDIyfQ.ySwvtbpSzdTimko0acSe03Tp6VadH1wCDhYxoNfgH3k'
 };
